7650573a5695600f67845b0b3f7fc1d57d755661
kismaneel <kismaneel@gmail.com>
2026-01-05 22:28:00 +0900

    playwright 설정 추가 및 테스트 코드 등록

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/button";
+import { buttonVariants } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function Home() {
@@ -6,11 +6,12 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-center gap-10">
       <h1 className="text-4xl font-bold">[전기차 충전맵]</h1>
       <div className="flex gap-2">
-        <Link href="/station">
-          <Button>충전소 검색</Button>
+        <Link href="/station" className={buttonVariants({ variant: "default" })}>
+          충전소 검색
         </Link>
-        <Link href="/favorite">
-          <Button>즐겨찾기</Button>
+
+        <Link href="/favorite" className={buttonVariants({ variant: "default" })}>
+          즐겨찾기
         </Link>
       </div>
     </main>
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,25 @@
-import { test, expect } from '@playwright/test';
+import { expect, test } from "@playwright/test";
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test.describe("라우팅 테스트", () => {
+  test("충전소 검색 페이지 이동", async ({ page }) => {
+    // 홈 페이지로 이동
+    await page.goto("http://localhost:3000/"); //
+    // 텍스트가 충전소 검색 버튼 찾아서 클릭
+    //await page.getByRole('link', { name: "충전소 검색" }).click();
+    // 가려져 있든, 아직 로딩 중이든 상관없이 강제로 눌러버리는 옵션입니다.
+    await page.getByRole('link', { name: "충전소 검색" }).click({ force: true });
+    // 새로운 URL이 '/station'인지 확인
+    await expect(page).toHaveURL("http://localhost:3000/station", { timeout: 10000 });
+  });
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  test("즐겨찾기 페이지 이동", async ({ page }) => {
+    // 홈 페이지로 이동
+    await page.goto("http://localhost:3000/");
+    // 텍스트가 즐겨찾기인 버튼 찾아서 클릭
+    //await page.getByRole('link', { name: "즐겨찾기" }).click();
+    // 가려져 있든, 아직 로딩 중이든 상관없이 강제로 눌러버리는 옵션입니다.
+    await page.getByRole('link', { name: "즐겨찾기" }).click({ force: true });
+    // 새로운 URL이 '/favorite'인지 확인
+    await expect(page).toHaveURL("http://localhost:3000/favorite", { timeout: 10000 });
+  });
 });
