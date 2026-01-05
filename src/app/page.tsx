3b88ccda51f5061078a75ce1bc1aed56a2525210
kismaneel <kismaneel@gmail.com>
2026-01-05 21:40:15 +0900

    vercel 자동 빌드 테스트용 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center gap-10">
-      <h1 className="text-4xl font-bold">전기차 충전맵</h1>
+      <h1 className="text-4xl font-bold">[전기차 충전맵]</h1>
       <div className="flex gap-2">
         <Link href="/station">
           <Button>충전소 검색</Button>
