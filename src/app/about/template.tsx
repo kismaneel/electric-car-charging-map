0b3e61114b0a2ef60f7ef558cfd0b87053035bdf
kismaneel <kismaneel@gmail.com>
2026-01-05 20:04:23 +0900

    tsx return 내용에 쌍따옴표 사용으로 인한 오류 수정 react/no-unescaped-entities

--- src/app/about/template.tsx
+++ src/app/about/template.tsx
@@ -1,7 +1,7 @@
 export default function Template({children}: {children: React.ReactNode}) {
     return (
         <div>
-            <div>"Template"</div>
+            <div>Template</div>
             {children}
         </div>
     );
--- src/app/key/page.tsx
+++ src/app/key/page.tsx
@@ -4,10 +4,10 @@ export default function KeyPage() {
     return (
         <div>
             <div>
-                "NEXT_PUBLIC_EV_CHARGING_API_KEY:" {process.env.NEXT_PUBLIC_EV_CHARGING_API_KEY}
+                NEXT_PUBLIC_EV_CHARGING_API_KEY: {process.env.NEXT_PUBLIC_EV_CHARGING_API_KEY}
             </div>
             <div>
-                "TEXT_KEY:" {process.env.TEXT_KEY} 
+                TEXT_KEY: {process.env.TEXT_KEY} 
             </div>
         </div>
     );
